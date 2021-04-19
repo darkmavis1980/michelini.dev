db61e5a5784df453aa9f654bee8f1627e1f4a3f0
Alessio Michelini <amichelini@hubspot.com>
2021-04-19 22:18:22 +0100

    first commit

---
+++ src/lib/Social.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  interface SocialMedia {
+    label: string;
+    url: string;
+    id: string;
+  }
+
+  const socials: SocialMedia[] = [
+    {
+      label: 'Linkedin',
+      url: '',
+      id: 'linkedin',
+    },
+    {
+      label: 'Twitter',
+      url: '',
+      id: 'twitter'
+    }
+  ];
+</script>
+
+<div id="social-media">
+  {#each socials as social}
+    <li>{social.label}</li>
+  {/each}
+</div>
