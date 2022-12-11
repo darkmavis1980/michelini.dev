591c8ba028f37cc76da0786a09ea42f70f1c55e7
Alessio Michelini <darkmavis1980@gmail.com>
2022-12-11 13:25:21 +0000

    Updated UI Updated dependencies

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,26 +1,36 @@
+<script lang="ts">
+  import Social from './Social.svelte';
+</script>
+
 <div class="card">
-  <div class="header">
-    <!-- <h1>Alessio Michelini <span>/ Web Dev</span></h1> -->
+  <header class="header">
     <img src="images/profile_3.jpg" alt="" class="profile">
-    <h1>Hi!</h1>
-  </div>
+    <h1>Alessio Michelini <span>/ Web Dev</span></h1>
+  </header>
+  <article>
+    <Social />
+  </article>
 </div>
 
-<style lang="scss">
+<style>
   .card {
     border-radius: 1rem;
-    padding: 3rem;
+    padding-top: 2rem;
     background-color: white;
     box-shadow: 5px 5px 30px -20px rgba(0,0,0,1);
     min-width: 1024px;
-    
-    .header {
-      img {
-        width: 300px;
-        height: 300px;
-        border-radius: 50px;
-        
-      }
-    }
+  }
+
+  .card .header img {
+    width: 300px;
+    height: 300px;
+    border-radius: 50px;
+  }
+
+  .card article {
+    background-color: var(--beige-color);
+    padding: 1rem;
+    border-bottom-right-radius: 1rem;
+    border-bottom-left-radius: 1rem;
   }
 </style>
--- src/lib/Social.svelte
+++ src/lib/Social.svelte
@@ -1,20 +1,33 @@
 <script lang="ts">
+  import linkedinImg from './icons/linkedin.svg';
+  import twitterImg from './icons/twitter.svg';
+  import devtoImg from './icons/devto.svg';
+
   interface SocialMedia {
     label: string;
     url: string;
     id: string;
+    image: string;
   }
 
   const socials: SocialMedia[] = [
     {
+      label: 'Dev.to',
+      url: 'https://dev.to/darkmavis1980',
+      id: 'devto',
+      image: devtoImg,
+    },
+    {
       label: 'Linkedin',
       url: 'https://www.linkedin.com/in/alessiomichelini/',
       id: 'linkedin',
+      image: linkedinImg,
     },
     {
       label: 'Twitter',
       url: 'https://twitter.com/darkmavis1980',
-      id: 'twitter'
+      id: 'twitter',
+      image: twitterImg,
     }
   ];
 </script>
@@ -22,26 +35,48 @@
 <ul id="social-media">
   {#each socials as social}
     <li id={social.id}>
-      <a href={social.url}>
+      <a href={social.url} target="_blank" rel="noreferrer">
+        <img src={social.image} alt={social.label} />
         {social.label}
       </a>
     </li>
   {/each}
 </ul>
 
-<style lang="scss">
-  ul#social-media {
+<style>
+  #social-media {
     margin: 0;
     padding: 0;
   }
 
-  ul#social-media > li {
+  #social-media > li {
     margin: 0;
     display: inline-block;
     padding: .25rem .75rem;
   }
 
-  li > a {
-    color: lightcoral;
+  #social-media > li > a {
+    color: black;
+    display: block;
+    padding: 0.5rem;
+    border-radius: 1rem;
+  }
+
+  #social-media > li > a:hover {
+    color: white;
+    /* border: 1px solid var(--navy-color); */
+    background: linear-gradient(0deg, var(--navy-color) 0%, var(--teal-color) 100%);
+  }
+
+  #social-media > li > a > img {
+    display: inline;
+  }
+
+  #social-media > li > a:hover > img {
+    filter: invert(100%);
+  }
+
+  #social-media img {
+    max-width: 40px;
   }
 </style>
